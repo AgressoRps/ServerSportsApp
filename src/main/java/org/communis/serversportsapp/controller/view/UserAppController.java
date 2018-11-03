82008ccc88672ee6355d86fe71d9547194ccedd9
AgressoRps <AgressoRj@gmail.com>
2018-11-03 22:07:35 +0300

    Add class UserAppController

---
+++ src/main/java/org/communis/serversportsapp/controller/view/UserAppController.java
@@ -0,0 +1,30 @@
+package org.communis.serversportsapp.controller.view;
+
+import lombok.extern.log4j.Log4j2;
+import org.communis.serversportsapp.dto.UserAppWrapper;
+import org.communis.serversportsapp.entity.UserApp;
+import org.communis.serversportsapp.exception.ServerException;
+import org.communis.serversportsapp.service.UserAppService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping("/user")
+public class UserAppController {
+    private final UserAppService userAppService;
+
+    @Autowired
+    public UserAppController(UserAppService userAppService){
+        this.userAppService = userAppService;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public UserAppWrapper getUserById(@PathVariable("id") Long id) throws ServerException{
+        return userAppService.getById(id);
+    }
+}
