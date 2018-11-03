d4c7393087e617fbee6e664d9264cc8abeee16c4
AgressoRps <AgressoRj@gmail.com>
2018-11-03 17:21:36 +0300

    Add Entity UserApp.java

--- src/main/java/org/communis/serversportsapp/AppBoot.java
+++ src/main/java/org/communis/serversportsapp/AppBoot.java
@@ -1,4 +1,4 @@
-package org.communis.javawebintro;
+package org.communis.serversportsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/org/communis/serversportsapp/entity/UserApp.java
@@ -0,0 +1,38 @@
+package org.communis.serversportsapp.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "user_app")
+public class UserApp {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "user_surname")
+    private String surname;
+
+    @Column(name = "user_login")
+    private String login;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role")
+    private UserAppRole role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state")
+    private UserState state;
+}
