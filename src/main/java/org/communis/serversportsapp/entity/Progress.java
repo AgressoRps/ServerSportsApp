07dfa53d88b3d79f245013ca90fa430fcf4eba4e
AgressoRps <AgressoRj@gmail.com>
2018-11-04 21:12:37 +0300

    Add new entity class Progress

---
+++ src/main/java/org/communis/serversportsapp/entity/Progress.java
@@ -0,0 +1,19 @@
+package org.communis.serversportsapp.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "progress")
+public class Progress {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "progress_name")
+    private String name;
+    
+}
