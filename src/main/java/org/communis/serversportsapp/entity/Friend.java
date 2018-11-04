3c61e29918834453c9ff84e32e1b06a846929a87
AgressoRps <AgressoRj@gmail.com>
2018-11-04 21:45:50 +0300

    Add new entity class Friend

---
+++ src/main/java/org/communis/serversportsapp/entity/Friend.java
@@ -0,0 +1,24 @@
+package org.communis.serversportsapp.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "friend")
+public class Friend {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_user")
+    private UserApp user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_friend")
+    private UserApp progress;
+
+}
