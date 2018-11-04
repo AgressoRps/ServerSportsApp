8b4906b7770421949e40c1fa96e83c926c3870b5
AgressoRps <AgressoRj@gmail.com>
2018-11-04 21:41:00 +0300

    Add entity class ProgressUser

---
+++ src/main/java/org/communis/serversportsapp/entity/ProgressUser.java
@@ -0,0 +1,24 @@
+package org.communis.serversportsapp.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "progress_user")
+public class ProgressUser {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_user")
+    private UserApp user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_progress")
+    private Progress progress;
+
+}
