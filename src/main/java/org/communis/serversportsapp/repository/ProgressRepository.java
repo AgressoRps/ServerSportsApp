0ebd77add5fa04ba239678132735d7ceca249a10
AgressoRps <AgressoRj@gmail.com>
2018-11-06 21:59:26 +0300

    Add new repository interface ProgressRepository

---
+++ src/main/java/org/communis/serversportsapp/repository/ProgressRepository.java
@@ -0,0 +1,16 @@
+package org.communis.serversportsapp.repository;
+
+import org.communis.serversportsapp.entity.Progress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProgressRepository extends JpaRepository<Progress, Short> {
+
+    /**
+     * Метод поиска и получения первого вхождения в таблицу указанного имени
+     * @param name наименование прогресса
+     * @return экземпляр класса Progress
+     */
+    Optional<Progress> findFirstByName(String name);
+}
