c5b69265c23140299cb722893b25abf26451c5c5
AgressoRps <AgressoRj@gmail.com>
2018-11-06 23:02:32 +0300

    Add new dto class RankWrapper

---
+++ src/main/java/org/communis/serversportsapp/dto/RankWrapper.java
@@ -0,0 +1,41 @@
+package org.communis.serversportsapp.dto;
+
+import lombok.Data;
+import org.communis.serversportsapp.entity.Rank;
+
+import java.io.Serializable;
+
+@Data
+public class RankWrapper implements ObjectWrapper<Rank>, Serializable {
+
+    private Short id;
+    private String name;
+
+    public RankWrapper(Rank rank){
+        toWrapper(rank);
+    }
+
+    /**
+     * Добавление данных объекта Rank в объект RankWrapper
+     * @param item - экземпляр объекта Rank
+     */
+    @Override
+    public void toWrapper(Rank item) {
+        if (item != null){
+            id = item.getId();
+            name = item.getName();
+        }
+    }
+
+    /**
+     * Получение допустимой информации об объекте, для отправки клиенту
+     * @param item - экземпляр объекта Rank, содержит только допустимые для отправки данные
+     */
+    @Override
+    public void fromWrapper(Rank item) {
+        if (item != null){
+            item.setId(id);
+            item.setName(name);
+        }
+    }
+}
