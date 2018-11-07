ac9cc90d9c10b8db93e64a9d514865d2574babe3
AgressoRps <AgressoRj@gmail.com>
2018-11-07 11:41:48 +0300

    Add new dto class TrainingLocationWrapper

---
+++ src/main/java/org/communis/serversportsapp/dto/TrainingLocationWrapper.java
@@ -0,0 +1,41 @@
+package org.communis.serversportsapp.dto;
+
+import lombok.Data;
+import org.communis.serversportsapp.entity.TrainingLocation;
+
+import java.io.Serializable;
+
+@Data
+public class TrainingLocationWrapper implements ObjectWrapper<TrainingLocation>, Serializable {
+
+    private Short id;
+    private String name;
+
+    public TrainingLocationWrapper(TrainingLocation trainingLocation){
+        toWrapper(trainingLocation);
+    }
+
+    /**
+     * Добавление данных объекта TrainingLocation в объект TrainingLocationWrapper
+     * @param item - экземпляр объекта TrainingLocation
+     */
+    @Override
+    public void toWrapper(TrainingLocation item) {
+        if (item != null){
+            id = item.getId();
+            name = item.getName();
+        }
+    }
+
+    /**
+     * Получение допустимой информации об объекте, для отправки клиенту
+     * @param item - экземпляр объекта TrainingLocation, содержит только допустимые для отправки данные
+     */
+    @Override
+    public void fromWrapper(TrainingLocation item) {
+        if (item != null){
+            item.setId(id);
+            item.setName(name);
+        }
+    }
+}
