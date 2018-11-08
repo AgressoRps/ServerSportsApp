1442f8d661b07d09d1ae0fe1f5bfe8928b66acd7
AgressoRps <AgressoRj@gmail.com>
2018-11-08 10:47:48 +0300

    Add new repository interface RankUserRepository

---
+++ src/main/java/org/communis/serversportsapp/repository/RankUserRepository.java
@@ -0,0 +1,20 @@
+package org.communis.serversportsapp.repository;
+
+import org.communis.serversportsapp.entity.RankUser;
+import org.communis.serversportsapp.entity.UserApp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface RankUserRepository extends JpaRepository<RankUser, Long> {
+
+    /**
+     * Метод поиска и получения всех данных связанных с переданным пользователем
+     * @param user пользователь приложения, экземпляр класса UserApp
+     * @return список экземпляров класса RankUser
+     */
+    @Query(value = "FROM RankUser rank WHERE rank.user =:user")
+    List<RankUser> findAllByUser(@Param("user") UserApp user);
+}
