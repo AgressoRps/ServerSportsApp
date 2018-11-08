6b976f64c362349da9c2222de908d8da8adeb8b3
AgressoRps <AgressoRj@gmail.com>
2018-11-08 08:21:44 +0300

    Add new repository interface FriendRepository

--- src/main/java/org/communis/serversportsapp/repository/FriendRepository.java
+++ src/main/java/org/communis/serversportsapp/repository/FriendRepository.java
@@ -1,6 +1,6 @@
 package org.communis.serversportsapp.repository;
 
-import org.communis.serversportsapp.entity.ProgressUser;
+import org.communis.serversportsapp.entity.Friend;
 import org.communis.serversportsapp.entity.UserApp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,13 +8,14 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ProgressUserRepository extends JpaRepository<ProgressUser, Long> {
+public interface FriendRepository extends JpaRepository<Friend, Long> {
 
     /**
      * Метод поиска и получения всех данных связанных с переданным пользователем
      * @param user пользователь приложения, экземпляр класса UserApp
-     * @return список экземпляров класса ProgressUser
+     * @return список экземпляров класса Friend
      */
-    @Query(value = "FROM ProgressUser progress WHERE progress.user =:user")
-    List<ProgressUser> findAllByUser(@Param("user") UserApp user);
+    @Query(value = "FROM Friend friend WHERE friend.user =:user")
+    List<Friend> findAllByUser(@Param("user") UserApp user);
+
 }
