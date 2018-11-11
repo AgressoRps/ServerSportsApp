1ea4d17fcfb8dad3704581df38337b9580b92dfd
AgressoRps <AgressoRj@gmail.com>
2018-11-11 23:12:57 +0300

    Changed repository interface TrainingLocationRepository, add comments and new methods

--- src/main/java/org/communis/serversportsapp/repository/TrainingLocationRepository.java
+++ src/main/java/org/communis/serversportsapp/repository/TrainingLocationRepository.java
@@ -3,6 +3,7 @@ package org.communis.serversportsapp.repository;
 import org.communis.serversportsapp.entity.TrainingLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TrainingLocationRepository extends JpaRepository<TrainingLocation, Short> {
@@ -13,4 +14,17 @@ public interface TrainingLocationRepository extends JpaRepository<TrainingLocati
      * @return экземпляр класса TrainingLocation
      */
     Optional<TrainingLocation> findFirstByName(String name);
+
+    /**
+     * Метод поиска и получения списка всех существующих тренировочных локаций
+     * @return список экземпляров класса TrainingLocation (список тренировочных локаций)
+     */
+    List<TrainingLocation> findAll();
+
+    /**
+     * Метод поиска и получения тренировочной локации по идентификатору
+     * @param id идентификатор тренировочной локации
+     * @return экземпляр класса TrainingLocation (тренировочная локация)
+     */
+    Optional<TrainingLocation> findById(Short id);
 }
