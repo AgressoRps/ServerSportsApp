bb998def962c84ba8251e152bdb97f437c157afd
AgressoRps <AgressoRj@gmail.com>
2018-11-11 22:38:38 +0300

    Changed repository interface TrainingDayRepository, add new methods

--- src/main/java/org/communis/serversportsapp/repository/TrainingDayRepository.java
+++ src/main/java/org/communis/serversportsapp/repository/TrainingDayRepository.java
@@ -5,13 +5,27 @@ import org.communis.serversportsapp.entity.TrainingProgram;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TrainingDayRepository extends JpaRepository<TrainingDay, Long> {
 
     /**
-     * Метод поиска списка тренировочных дней переданной тренировочной программы
-     * @param trainingProgram экземпляр класса тренировочной программы
-     * @return список экземпляров класса TrainingDay
+     * Метод поиска списка тренировочных дней по идентификатору тренировочной программы
+     * @param id идентификатор тренировочной программы
+     * @return список экземпляров класса TrainingDay (список тренировочных дней)
      */
-    List<TrainingDay> findByTrainingProgram(TrainingProgram trainingProgram);
+    List<TrainingDay> findAllByTrainingProgramID(Long id);
+
+    /**
+     * Метод поиска всех тренировочных дней
+     * @return список экземпляров класса TrainingDay (список всех тренировочных дней)
+     */
+    List<TrainingDay> findAll();
+
+    /**
+     * Метод поиска тренировочного дня по переданному идентификатору
+     * @param id идентификатор тренировочного дня
+     * @return экземпляр класса TrainingDay (тренировочный день)
+     */
+    Optional<TrainingDay> findById(Long id);
 }
