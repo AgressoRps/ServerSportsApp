a6c70846ec6694af5a24c0ae929db6f7864a98d7
AgressoRps <AgressoRj@gmail.com>
2018-11-12 10:34:22 +0300

    Changed repository interface UserAppRepository

--- src/main/java/org/communis/serversportsapp/repository/UserAppRepository.java
+++ src/main/java/org/communis/serversportsapp/repository/UserAppRepository.java
@@ -8,12 +8,28 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserAppRepository extends JpaRepository<UserApp, Long> {
+
     /**
      * Метод поиска пользователя по логину
      * @param login логин пользователя, которого нужно найти
      * @return экземпляр класса UserApp
      */
-    Optional<UserApp> findFirstByLogin(String login);
+    Optional<UserApp> findByLogin(String login);
+
+    /**
+     * Метод поиска пользователя по электронной почте
+     * @param email почта пользователя, которого нужно найти
+     * @return экземпляр класса UserApp
+     */
+    Optional<UserApp> findByEmail(String email);
+
+    /**
+     * Метод поиска пользователя по имени либо логину
+     * @param name имя пользователя
+     * @param login логин пользователя
+     * @return список экземпляров класса UserApp (список пользователей)
+     */
+    List<UserApp> findAllByNameLikeOrLoginLike(String name, String login);
 
     /**
      * Метод поиска пользователей по роли
