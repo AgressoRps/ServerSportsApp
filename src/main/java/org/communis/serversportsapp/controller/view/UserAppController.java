62325fb3565b00816baacc79ffb5bcf788779fd1
AgressoRps <AgressoRj@gmail.com>
2018-11-12 12:57:34 +0300

    Changed controller class UserAppController, add comments and new methods

--- src/main/java/org/communis/serversportsapp/controller/view/UserAppController.java
+++ src/main/java/org/communis/serversportsapp/controller/view/UserAppController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Log4j2
 @RestController
 @RequestMapping("/user")
@@ -24,7 +26,8 @@ public class UserAppController {
     }
 
     /**
-     * Метод получения данных указанного пользователя по идентификатору
+     * Метод реагирует на запрос /user/{id}, выполняет запрос к бд
+     * для получения данных указанного пользователя по идентификатору
      * @param id идентификатор запрашиваемого пользователя
      * @return UserAppWrapper - содержит допустимые для отправки данные о пользователе
      * @throws ServerException в случае ошибки во время выполнения - генерация исключения
@@ -32,5 +35,65 @@ public class UserAppController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public UserAppWrapper getUserById(@PathVariable("id") Long id) throws ServerException{
         return userAppService.getById(id);
+    }
+
+    /**
+     * Метод реагирует на запрос /user/login/{login}, выполняет запрос к бд
+     * для получения данных указанного пользователя по логину
+     * @param login логин пользователя
+     * @return экземпляр класса UserAppWrapper (пользователь)
+     * @throws ServerException в случае ошибки генерирует исключение
+     */
+    @RequestMapping(value = "/login/{login}", method = RequestMethod.GET)
+    public UserAppWrapper getUserByLogin(@PathVariable("login") String login) throws ServerException{
+        return userAppService.getByLogin(login);
+    }
+
+    /**
+     * Метод реагирует на запрос /user/email/{email}, выполняет запрос к бд
+     * для получения данных указанного пользователя по электронной почте
+     * @param email электронная почта пользователя
+     * @return экземпляр класса UserAppWrapper (пользователь)
+     * @throws ServerException в случае ошибки генерирует исключение
+     */
+    @RequestMapping(value = "/email/{email}", method = RequestMethod.GET)
+    public UserAppWrapper getUserByEmail(@PathVariable("email") String email) throws ServerException{
+        return userAppService.getByEmail(email);
+    }
+
+    /**
+     * Метод реагирует на запрос /user/login-email/{value}, выполняет запрос к бд
+     * для поиска пользователя по логину либо электронной почте
+     * @param value логин либо электронная почта
+     * @return экземпляр класса UserAppWrapper (пользователь)
+     * @throws ServerException в случае ошибки генерирует исключение
+     */
+    @RequestMapping(value = "/login-email/{value}", method = RequestMethod.GET)
+    public UserAppWrapper getUserByLoginOrEmail(@PathVariable("value") String value) throws ServerException{
+        return userAppService.getByLoginOrEmail(value, value);
+    }
+
+    /**
+     * Метод реагирует на запрос /user/all/name-login/{email}, выполняет запрос к бд
+     * для поиска списка пользователей соответствующих переданному имени или логину
+     * @param value имя либо логин пользователя
+     * @return список экземпляров класса UserAppWrapper (список найденных пользователей)
+     * @throws ServerException в случае ошибки генерирует исключение
+     */
+    @RequestMapping(value = "/all/name-login/{value}", method = RequestMethod.GET)
+    public List<UserAppWrapper> getAllUsersByNameOrLogin(@PathVariable("value") String value) throws ServerException{
+        return userAppService.getByNameLikeOrLoginLike(value, value);
+    }
+
+    /**
+     * Метод реагирует на запрос /user/role/{role}, выполняет запрос к бд
+     * для поиска списка пользователей соответствующих переданной роли
+     * @param role роль пользователя
+     * @return список экземпляров класса UserAppWrapper (список найденных пользователей)
+     * @throws ServerException в случае ошибки генерирует исключение
+     */
+    @RequestMapping(value = "/role/{role}", method = RequestMethod.GET)
+    public List<UserAppWrapper> getAllUsersByRole(@PathVariable("role") String role) throws ServerException{
+        return userAppService.getByRole(role);
     }
 }
